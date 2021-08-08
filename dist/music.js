1e4d6a1311533ea23cd35fe7233e1a637f3b2765
244099121 <244099121@qq.com>
2021-08-08 18:43:16 +0800

    Site updated: 2021-08-08 18:43:16

---
+++ dist/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD���֮��",
+        artist: '����Բ',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: 'ҹ���� (Night Voyager)',
+        artist: '���� / ��������',
+        url: 'https://m10.music.126.net/20210808185052/9c59dd9a98364c8a624fe9e0ca1d2c9f/yyaac/obj/wonDkMOGw6XDiTHCmMOi/1714002413/2302/18ba/cb15/8a0f7c47cec1cfc754e32f38a66e86c9.m4a',
+        cover: 'http://p1.music.126.net/EIJc2Wt5AEsIHCFcVxm6pg==/109951165033567317.jpg?param=130y130',
+      },
+      {
+        name: '������',
+        artist: '������',
+        url: 'https://m10.music.126.net/20210808185314/ff51a8ba50ec21fbc4d9eb18737a4d6f/yyaac/obj/wonDkMOGw6XDiTHCmMOi/2102774316/f8f7/40fc/23e3/0492faadd616be2adb3927175537efe4.m4a',
+        cover: 'http://p2.music.126.net/gyLyNel6O5vab9omu5r5pw==/109951164886664565.jpg?param=130y130',
+      }
+    ]
+});
